435eefd4030a4199d90965332b80787285a13e22
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-03-11 02:29:09 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//falsy
+//false
+//0
+//""
+//udenfined
+//null
+//NaN
+// truthy
+// "0"
+// " "
+//[]
+
+const name = 0;
+if(name || name == 0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
